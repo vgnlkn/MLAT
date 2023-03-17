73e2dce2eca319efd8b9f0b9c60591d99e0d2c98
TapekhaVA <tapehavladimir@gmail.com>
2023-03-17 15:34:31 +0300

    OurVector: some corrections

--- libraries/math/vector.h
+++ libraries/math/vector.h
@@ -10,7 +10,7 @@
 /*! \class OurVector
 *   \brief Class describing the custom vector
 */
-template<uint8_t size, typename type = float>
+template<uint8_t size, typename type=float>
 class OurVector final
 {
 public:
@@ -18,59 +18,50 @@ public:
     OurVector() : _vector(new type[size]) { setValue(0.f); }
     //! Copy constructor
     OurVector(const OurVector& other);
-    //! Overloading operator =
+    //! Overloading operator=
     OurVector<size, type>& operator=(const OurVector& other);
     //! Destructor
-    ~OurVector();
-    //! set vector values to val
+    ~OurVector() { if (_vector) { delete[] _vector; } };
+
+    //! Set vector values to val
     void setValue(type value);
     //! Replace all fields
-    void swap(OurVector& other);
-    //! Overloading operator==
-    bool operator==(const OurVector& other);
-    //! Overloading operator!=
-    bool operator!=(const OurVector& other);
+    void swap(OurVector& other) { std::swap(this->_vector, other._vector); };
+    //! Get size
+    [[nodiscard]] uint8_t inline getSize() const { return size; }
 
+    //! OPERATOR OVERLOADINGS
+    //! Overloading operator==
+    bool operator==(const OurVector& other) const;
+    //! Overloading operator!=
+    bool operator!=(const OurVector& other) const;
     //! Overloading operator[] with link
     type& operator[](uint8_t i) const { return _vector[i]; }
-
     //! Overloading sum with another _vector
-    OurVector<size, type> operator+(const OurVector& other);
+    OurVector<size, type> operator+(const OurVector& other) const;
     //! Overloading multiplying with another _vector
-    OurVector<size, type> operator*(const OurVector& other);
+    OurVector<size, type> operator*(const OurVector& other) const;
     //! Overloading subtraction with another _vector
-    OurVector<size, type> operator-(OurVector other);
+    OurVector<size, type> operator-(OurVector other) const;
     //! Overloading operator- that inverts number
-    OurVector<size, type> operator-();
-
+    OurVector<size, type> operator-() const;
     //! Overloading multiplying with single number
-    OurVector<size, type> operator*(float number);
+    OurVector<size, type> operator*(float number) const;
     //! Overloading multiplying with single number(number is first)
     template<uint8_t size_, typename T>
     friend OurVector<size_, T> operator*(float number, OurVector<size_, T>& vector);
-
     //! Overloading output to stream operator
     template<uint8_t size_, typename T>
     friend std::ostream& operator<<(std::ostream& os, const OurVector<size_, T>& vector);
 
-    //! Get size
-    [[nodiscard]] uint8_t inline getSize() const { return size; }
 private:
     //! Check all parameters
-    void checkParams(const OurVector& other);
+    void checkParams(const OurVector& other) const;
 
+private:
     type* _vector;
 };
 
-
-template<uint8_t size, typename type>
-OurVector<size, type>::~OurVector()
-{
-    if (_vector)
-    {
-        delete[] _vector;
-    }
-}
 
 template<uint8_t size, typename type>
 inline void OurVector<size, type>::setValue(type value)
@@ -82,7 +73,7 @@ inline void OurVector<size, type>::setValue(type value)
 }
 
 template<uint8_t size, typename type>
-OurVector<size, type> OurVector<size, type>::operator*(float number)
+OurVector<size, type> OurVector<size, type>::operator*(float number) const
 {
     OurVector<size, type> result;
 
@@ -98,12 +89,6 @@ template<uint8_t size_, typename T>
 OurVector<size_, T> operator*(float number, OurVector<size_, T>& vector)
 {
     return vector * number;
-}
-
-template<uint8_t size, typename type>
-void OurVector<size, type>::swap(OurVector &other)
-{
-    std::swap(this->_vector, other._vector);
 }
 
 template<uint8_t size, typename type>
@@ -130,7 +115,7 @@ OurVector<size, type>::OurVector(const OurVector &other) : _vector(new type[size
 }
 
 template<uint8_t size, typename type>
-bool OurVector<size, type>::operator==(const OurVector &other)
+bool OurVector<size, type>::operator==(const OurVector &other) const
 {
     checkParams(other);
 
@@ -146,13 +131,13 @@ bool OurVector<size, type>::operator==(const OurVector &other)
 }
 
 template<uint8_t size, typename type>
-bool OurVector<size, type>::operator!=(const OurVector &other)
+bool OurVector<size, type>::operator!=(const OurVector &other) const
 {
     return !(*this == other);
 }
 
 template<uint8_t size, typename type>
-OurVector<size, type> OurVector<size, type>::operator+(const OurVector &other)
+OurVector<size, type> OurVector<size, type>::operator+(const OurVector &other) const
 {
     checkParams(other);
     OurVector<size, type> result;
@@ -166,20 +151,20 @@ OurVector<size, type> OurVector<size, type>::operator+(const OurVector &other)
 }
 
 template<uint8_t size, typename type>
-OurVector<size, type> OurVector<size, type>::operator-()
+OurVector<size, type> OurVector<size, type>::operator-() const
 {
     return *this * (-1);
 }
 
 template<uint8_t size, typename type>
-OurVector<size, type> OurVector<size, type>::operator-(OurVector other)
+OurVector<size, type> OurVector<size, type>::operator-(OurVector other) const
 {
     return *this + (-other);
 }
 
 
 template<uint8_t size, typename type>
-OurVector<size, type> OurVector<size, type>::operator*(const OurVector& other)
+OurVector<size, type> OurVector<size, type>::operator*(const OurVector& other) const
 {
     checkParams(other);
     OurVector<size, type> result;
@@ -204,7 +189,7 @@ std::ostream &operator<<(std::ostream &os, const OurVector<size_, T> &vector)
 }
 
 template<uint8_t size, typename type>
-void OurVector<size, type>::checkParams(const OurVector &other)
+void OurVector<size, type>::checkParams(const OurVector &other) const
 {
     assert(std::is_arithmetic<type>() &&
            typeid(_vector).name() == typeid(other._vector).name());
