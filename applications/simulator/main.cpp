436b426300ea4e7cee75fec6f4960d95cc466ab1
vgnlkn <vgn1lkn26@gmail.com>
2023-03-23 21:17:16 +0300

    - added visualisation for simulation

--- applications/simulator/main.cpp
+++ applications/simulator/main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <plotter.h>
+#include <vector.h>
+#include <aircraft.h>
+#include <field.h>
 
 #define RAND_ (float)(rand()) / RAND_MAX
 
@@ -9,7 +12,7 @@ int main()
 {
 	std::string filepath = STOCK_PATH;
 	Plotter plt(filepath);
-	char c;
+	/*char c;
 	for (int i = 0; i < 100; ++i)
 	{
 		float x = RAND_;
@@ -17,7 +20,21 @@ int main()
 		float z = RAND_;
 		std::cin >> c;
 		plt.addPoint(x, y, z);
-		
+	}*/
+
+	OurVector<3> a;
+	a.setValue(1);
+
+	Aircraft aircraft(a);
+	aircraft.calculateNewSpeed();
+	Field obj(aircraft);
+	obj.startMovement();
+
+	OurVector<3> cord;
+	for (int i = 0; i < 1000; ++i)
+	{
+		cord = obj.getCoordinate(i);
+		plt.addPoint(cord[0], cord[1], cord[2]);
 	}
 	
 	return 0;
