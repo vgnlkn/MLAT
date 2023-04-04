056c2a809cb17005415f23c09ccf94091709abbc
TapekhaVA <tapehavladimir@gmail.com>
2023-04-05 00:38:21 +0300

    rewrite scripts again

--- run-plotter.sh
+++ run-plotter.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python libraries/aircraft-simulator/plotter.py libraries/aircraft-simulator/plotter.txt applications/simulator/plotter.txt
+python3 libraries/aircraft-simulator/plotter.py libraries/aircraft-simulator/plotter.txt applications/simulator/plotter.txt
