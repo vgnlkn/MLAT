701900c8c1ba99eb3f7cdfcebd6b6388d664685a
TapekhaVA <tapehavladimir@gmail.com>
2023-05-31 02:21:06 +0300

    commit

--- libraries/aircraft-simulator/src/field.cpp
+++ libraries/aircraft-simulator/src/field.cpp
@@ -72,7 +72,7 @@ void Field::updatePlot()
     {
         if (plt)
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            std::this_thread::sleep_for(std::chrono::milliseconds(2));
             plt->addPoint(param[0], param[1], param[2]);
         }
     };
