52020ab3f8b277f29d6ee558330c3e935411603e
TapekhaVA <tapehavladimir@gmail.com>
2023-05-30 21:26:54 +0300

    it works

--- libraries/aircraft-simulator/plotter.py
+++ libraries/aircraft-simulator/plotter.py
@@ -62,7 +62,7 @@ def animate(i):
     ax.clear()
     ax.plot(xs1, ys1, zs1, label='MLAT', color='blue')
     ax.plot(xs2, ys2, zs2, label='Real values', color='red')
-    ax.plot(xs3, ys3, zs3, label='Inverse problem', color='black')
+    ax.plot(xs3, ys3, zs3, label='Kalman Filter', color='black')
 
     ax.set_xlabel('x, km')
     ax.set_ylabel('y, km')
@@ -166,5 +166,68 @@ axs[2][2].set_title('a_z')
 axs[2][2].set_ylabel('m/s^2')
 axs[2][2].set_xlabel('iteration number')
 axs[2][2].legend()
+
+plt.show()
+
+
+def count_diff(first_arr: list, second_arr: list) -> list:
+    diff = []
+
+    for i in range(min(len(first_arr), len(second_arr))):
+        diff.append(abs(first_arr[i] - second_arr[i]))
+
+    return diff
+
+
+fig, axs = plt.subplots(3, 3, figsize=(8, 10))
+
+
+# Coordinates
+# Plot for x
+axs[0][0].plot(count_diff(arr_x_real, arr_x_filter))
+axs[0][0].set_title('x')
+axs[0][0].set_ylabel('km')
+
+# Plot for y
+axs[1][0].plot(count_diff(arr_y_real, arr_y_filter))
+axs[1][0].set_title('y')
+axs[1][0].set_ylabel('km')
+
+# Plot for z
+axs[2][0].plot(count_diff(arr_z_real, arr_z_filter))
+axs[2][0].set_title('z')
+axs[2][0].set_ylabel('km')
+
+# Speed
+# Plot for x
+axs[0][1].plot(count_diff(x_real_speed, x_filter_speed))
+axs[0][1].set_title('v_x')
+axs[0][1].set_ylabel('m/s')
+
+# Plot for y
+axs[1][1].plot(count_diff(y_real_speed, y_filter_speed))
+axs[1][1].set_title('v_y')
+axs[1][1].set_ylabel('m/s')
+
+# Plot for z
+axs[2][1].plot(count_diff(z_real_speed, z_filter_speed))
+axs[2][1].set_title('v_z')
+axs[2][1].set_ylabel('m/s')
+
+# Acceleration
+# Plot for x
+axs[0][2].plot(count_diff(x_real_acceleration, x_filter_acceleration))
+axs[0][2].set_title('a_x')
+axs[0][2].set_ylabel('m/s^2')
+
+# Plot for y
+axs[1][2].plot(count_diff(y_real_acceleration, y_filter_acceleration))
+axs[1][2].set_title('a_y')
+axs[1][2].set_ylabel('m/s^2')
+
+# Plot for z
+axs[2][2].plot(count_diff(z_real_acceleration, z_filter_acceleration))
+axs[2][2].set_title('a_z')
+axs[2][2].set_ylabel('m/s^2')
 
 plt.show()
