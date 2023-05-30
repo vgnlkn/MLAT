8b2b00d1d21197ac8afeda381591b3c173ecb94c
TapekhaVA <tapehavladimir@gmail.com>
2023-05-30 20:40:01 +0300

    it works

--- libraries/aircraft-simulator/plotter.py
+++ libraries/aircraft-simulator/plotter.py
@@ -100,21 +100,21 @@ fig, axs = plt.subplots(3, 3, figsize=(8, 10))
 # Plot for x coordinate
 axs[0][0].plot(arr_x_real, label='Real')
 axs[0][0].plot(arr_x_filter, label='Filter')
-axs[0][0].set_title('Coordinates of the aircraft on the axis x')
+axs[0][0].set_title('x')
 axs[0][0].set_ylabel('km')
 axs[0][0].legend()
 
 # Plot for y coordinate
 axs[1][0].plot(arr_y_real, label='Real')
 axs[1][0].plot(arr_y_filter, label='Filter')
-axs[1][0].set_title('Coordinates of the aircraft on the axis y')
+axs[1][0].set_title('y')
 axs[1][0].set_ylabel('km')
 axs[1][0].legend()
 
 # Plot for z coordinate
 axs[2][0].plot(arr_z_real, label='Real')
 axs[2][0].plot(arr_z_filter, label='Filter')
-axs[2][0].set_title('Coordinates of the aircraft on the axis z')
+axs[2][0].set_title('z')
 axs[2][0].set_ylabel('km')
 axs[2][0].set_xlabel('iteration number')
 axs[2][0].legend()
@@ -124,21 +124,21 @@ axs[2][0].legend()
 # Plot for x speed
 axs[0][1].plot(x_real_speed, label='Real')
 axs[0][1].plot(x_filter_speed, label='Filter')
-axs[0][1].set_title('x speed')
+axs[0][1].set_title('v_x')
 axs[0][1].set_ylabel('m/s')
 axs[0][1].legend()
 
 # Plot for y speed
 axs[1][1].plot(y_real_speed, label='Real')
 axs[1][1].plot(y_filter_speed, label='Filter')
-axs[1][1].set_title('y speed')
+axs[1][1].set_title('v_y')
 axs[1][1].set_ylabel('m/s')
 axs[1][1].legend()
 
 # Plot for z speed
 axs[2][1].plot(z_real_speed, label='Real')
 axs[2][1].plot(z_filter_speed, label='Filter')
-axs[2][1].set_title('z speed')
+axs[2][1].set_title('v_z')
 axs[2][1].set_ylabel('m/s')
 axs[2][1].set_xlabel('iteration number')
 axs[2][1].legend()
@@ -148,21 +148,21 @@ axs[2][1].legend()
 # Plot for x speed
 axs[0][2].plot(x_real_acceleration, label='Real')
 axs[0][2].plot(x_filter_acceleration, label='Filter')
-axs[0][2].set_title('x acceleration')
+axs[0][2].set_title('a_x')
 axs[0][2].set_ylabel('m/s^2')
 axs[0][2].legend()
 
 # Plot for y speed
 axs[1][2].plot(y_real_acceleration, label='Real')
 axs[1][2].plot(y_filter_acceleration, label='Filter')
-axs[1][2].set_title('y acceleration')
+axs[1][2].set_title('a_y')
 axs[1][2].set_ylabel('m/s^2')
 axs[1][2].legend()
 
 # Plot for z speed
 axs[2][2].plot(z_real_acceleration, label='Real')
 axs[2][2].plot(z_filter_acceleration, label='Filter')
-axs[2][2].set_title('z acceleration')
+axs[2][2].set_title('a_z')
 axs[2][2].set_ylabel('m/s^2')
 axs[2][2].set_xlabel('iteration number')
 axs[2][2].legend()
